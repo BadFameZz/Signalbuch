6501e86b00a5104734d08128740f7245623251c1
BadFameZz <81498380+BadFameZz@users.noreply.github.com>
2025-10-08 21:27:04 +0200

    Login-Container Breite rückgängig gemacht

--- sw.js
+++ sw.js
@@ -11,11 +11,11 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
   );
+});
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-});
 });
